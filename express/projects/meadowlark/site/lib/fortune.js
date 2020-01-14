c852f99592d59363604bac827df178f67ebfaaf3
kymin <kymin@ebay.com>
2020-01-15 08:38:39 +0900

    파일정리

--- express/projects/meadowlark/site/lib/fortune.js
+++
@@ -1,5 +0,0 @@
-var fortuneCookies = ["기쁘다", "화난다", "슬프다", "즐겁다"];
-exports.getFortune = function() {
-  var idx = Math.floor(Math.random() * fortuneCookies.length);
-  return fortuneCookies[idx];
-};
--- node/fast.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const obj = {
-    title : 'node.js 올인원 패키지'
-}
-
-const isEmptyObj = () => obj.title ? true : false
-
-isEmptyObj()
