6b8c524c0018d11946b312c9b3bf61ab384ecaee
minkh0512 <minkh0512@naver.com>
2020-01-17 20:21:28 +0900

    node

--- egoing/NodeJS/main.js
+++ egoing/NodeJS/main.js
@@ -8,9 +8,7 @@ var app = http.createServer(function(request, response) {
   var pathName = url.parse(_url, true).pathname;
  
   if(pathName === '/'){
-    console.log('has pathname' + queryData.id);
     if(queryData.id === undefined){
-      console.log('has not data id');
       fs.readdir('./data', function(error, filelist){
         var title = 'Welcome';
         var description = 'Hello, Node.js';
@@ -36,17 +34,15 @@ var app = http.createServer(function(request, response) {
           </body>
           </html>
         `;
-
         response.writeHead(200);
         response.end(template);
       });
     }else{
-      console.log('has data id' + queryData.id);
       fs.readdir('./data', function(error, filelist){
         var list = '<ul>';
         var i = 0;
         while(i < filelist.length){
-          list = list + `<li><a href="/?id="${filelist[i]}">${filelist[i]}</a></li>`;
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
           i++;
         }
         list = list + '</ul>';
