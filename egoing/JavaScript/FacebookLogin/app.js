d84a51569421340f2fdeafb39649427a9cbd2ad9
kymin <kymin@ebay.com>
2020-01-31 09:39:31 +0900

    fb login

---
+++ egoing/JavaScript/FacebookLogin/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+const app = http.createServer((request, response) => {
+  let url = request.url;
+  if(url == '/'){
+    url = '/index.html';
+  }
+  if(url == '/favicon.ico'){
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  response.end(fs.readFileSync(__dirname + url));
+});
+
+app.listen(3000);
