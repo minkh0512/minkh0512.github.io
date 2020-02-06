e64c508a224afc9637a087a4e5fbe19c5febfa5a
kymin <kymin@ebay.com>
2020-02-06 16:28:41 +0900

    todo

--- MyProjects/JavaScript/TodoList/js/app.js
+++ MyProjects/JavaScript/TodoList/js/app.js
@@ -2,24 +2,32 @@ const todoForm = document.querySelector('.form__todo'),
     todoInput = todoForm.querySelector('.input__todo'),
     todoInputButton = todoForm.querySelector('.button__submit'),
     todoListWrap = todoForm.querySelector('.list__todo');
-    
 
-
+function deleteTodo(event){
+    console.log(event)
+}
 function paintTodoList(todoText){
-    return todoListWrap.insertAdjacentHTML(
+    todoListWrap.insertAdjacentHTML(
         'beforeend',
-        `<div>${todoText}</div>`,
+        `<li class="list-item">
+            <input type="checkbox" id="" class="input__text" />
+            <label for="" class="label">${todoText}</label>
+            <div class="box__button">
+                <button type="button" class="button__modify" onclick=${deleteTodo(event)}><i class="fa fa-edit"></i><span class="for-a11y">수정</span></button>
+                <button type="button" class="button__delete"><i class="fa fa-trash"></i><span class="for-a11y">삭제</span></button>
+            </div>
+        </li>`,
     );
+    console.log(buttonDelete);
 }
 
 function handleSubmit(event){
     event.preventDefault();
-    paintTodoList('ttt');
-    console.log('submit');
+    const inputValue = todoInput.value;
+    paintTodoList(inputValue);
 }
 
 function init(){
-    console.log('init');
     todoForm.addEventListener('submit', handleSubmit);
     todoInputButton.addEventListener('click', handleSubmit);
 }
